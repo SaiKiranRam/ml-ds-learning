417be019f6e3a33ed6261120baf29a0caa268332
SaiKiranRam <>
2025-08-02 11:05:25 +0530

    Day1 Python Intro

---
+++ Week1_Introduction_To_Python/Day1/P2_Input_And_Output.py
@@ -0,0 +1,66 @@
+# # Taking an input and printing it
+# name = input("Hello There This is an AI ML course python prompt. Whats your name by the way?")
+# # Note that below print function doesn't explicitly have spaces between the usual string and variables since
+# # they're comma separated, the python will automatically take care of the spaces
+# print("Welcome,", name, "! Let's jump into the course and create new apps 😄💪")
+#
+#
+# name = input("Please enter the name of your headache: ")
+# # For String/text, typecasting isn't required as Python will automatically consider the input as String/text
+# # Typecasting input with int() converts the data into int type
+# age = int(input("Please enter headache's age: "))
+# city = input("Enter headache's city: ")
+# print("Details you've entered are :")
+# print(name, age, city) # Note that the variables are only comma separated. Bellow will be formatted for brevity
+# print("Name:", name)
+# print("Age:", age)
+# print("City:", city)
+from reprlib import aRepr
+
+# ______________________________________________________________________________________________________________________
+
+# Taking multiple space-separated inputs from console and printing it
+x, y = input("Enter number of boys and girls in your class respectively:").split()
+print() # By default, prints new line
+boysNumber = "Number of boys:", x
+girlsNumber = "Number of girls:", y
+
+# Python has a provision to print multiple strings with a common separator which is appended between these strings
+# Similarly, we can also define an end text which is executed after executing all the given Strings
+# By default value of separator - sep is a single whitespace and of end is a new line \n
+print(boysNumber, girlsNumber, sep='\n', end='\n_________________________________________\n')
+
+# EXAMPLE 2
+# date, month, year = input("Enter the date, month and year separated by a space:").s
+# print(date, month, year, sep='-', end='\n\n')
+
+
+# ______________________________________________________________________________________________________________________
+
+# Type casting input or given values
+# We can convert a single string input into int or any proper data type by casting it over input function OR
+# by casting it over assigned variable
+z = int(input("Enter total number of students, according to you:"))
+totalNumber = int(x) + int(y)
+print("Entered number of students:", z, "\nWhile the actual total is:", totalNumber)
+
+# Typecasting the input/int/string to float using float() function
+boysRatio = float(int(x) / totalNumber)
+print("Ratio of boys to to total students in decimals:", boysRatio) # Prints boysRatio as 0.3333333333333333
+print("Formatting above decimal result into nearest 2 decimals: ${:.2f}".format(boysRatio))
+
+
+# Using f-string
+# We can use f-string by placing f"...{var1}....{var2}..." within print function argument where var1 and var2 are
+# already assigned variables which needs to be logged
+#Based on the above count of x (boys) and girls (y) using the same variables as example
+print(f"With f-String we know that in the class there are {x} boys and {y} girls")
+
+# Using % operator
+# %d - integer
+# %f - float
+# %s - string
+# %x - hexadecimal
+# %o - octal
+
+print("Total number of students are: %d" %totalNumber, "in the class")
---
+++ Week1_Introduction_To_Python/Day1/P3_Variables.py
@@ -0,0 +1,43 @@
+# Variables naming Convention
+    # Variable names can only contain letters, digits and underscores (_).
+    # A variable name cannot start with a digit.
+    # Variable names are case-sensitive (myVar and myvar are different).
+    # Avoid using Python keywords (e.g., if, else, for) as variable names.
+
+# Type Casting functions:
+#     int() - Converts compatible values to an integer.
+#     float() - Transforms values into floating-point numbers.
+#     str() - Converts any data type into a string.
+
+num = 5
+numFloat = float(num)
+numWord = str(num)
+print("num:", num)
+print("numFloat:", numFloat)
+print("numWord:", numWord)
+print("Integer addition with var num:", num + num)
+print("Float addition with var numFloat:", numFloat + numFloat)
+print("String addition with var numWord:", numWord + numWord)
+
+# Know the type of vairable using type() function
+# Simple example:
+print("Type of 100 is: ", end='')
+print(type(100), end='\n\n')
+#Example # 2
+print("Type of:", f"num: %s" %type(num), f"numFloat: %s" %type(numFloat), f"numWord: %s" %type(numWord), sep='\n')
+
+
+# Unlike Java, Object reference in python works by allocating the value's reference directly to variable,
+# if a variable is used to assign another variable even in that case the latest variable has direct access
+# to the value instead of the former variable. For example:
+a = 5
+b = a
+print("Value of a:", a, "and b:", b, "BEFORE CHANGES in a")
+a = 10
+print("Value of a:", a, "and b:", b, "AFTER CHANGES in a")
+
+# We can delete a variable using del() function. Example below:
+del(a)
+# After deleting if we try to access the same variable, we get NameError: name 'a' is not defined
+# Uncomment below print function to verify the same
+# print(a).
